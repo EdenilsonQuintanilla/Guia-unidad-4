e6c72e2e76667688f32373308a3fc52a402a7cc4
Edenilson David Alexis Quintanilla Medrano <48040961+EdenilsonQuintanilla@users.noreply.github.com>
2019-05-21 21:27:47 -0600

    Add files via upload

---
+++ Solution1/capaNegocio/ManttVendor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+namespace capaNegocio
+{
+    public class ManttVendor
+    {
+        bdConex test= new bdConex();
+        public String prueba()
+        {
+            String mensaje;
+            if (test.Conectar() == true)
+            {
+                test.Desconectar();
+                mensaje = "Conexion realizada";
+            }
+            else {
+                test.Desconectar();
+                mensaje = "Conexion no realizada";
+            }
+            return mensaje;
+
+        }
+    }
+}
